f7d2a3be497bbe4d88dcec8e3171d09efeb61ab4
wqf321 <43094170+wqf321@users.noreply.github.com>
2020-02-10 12:15:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 from DataLoad import MyDataset
 from Model import MMGCN
 from MMGraphSAGE import MMGraphSAGE
+from Full_vt import full_vt
 import pdb
 import sys
 
@@ -78,7 +79,7 @@ class Logger(object):
 class Net:
     def __init__(self, args):
         ##########################################################################################################################################
-        self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         ##########################################################################################################################################
         self.model_name = args.model_name
         self.data_path = args.data_path
@@ -103,17 +104,24 @@ class Net:
         self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
         self.edge_index = np.load('./Data/train.npy')
         self.user_index_5 = np.load('./Data/edge_adj_12.npy')
-        self.val_dataset = np.load('./Data/val.npy')
-        self.test_dataset = np.load('./Data/test.npy')
-        self.val_test_dataset = np.load('./Data/val_test.npy')
+        self.val_dataset = np.load('./Data/val_full.npy')
+        self.test_dataset = np.load('./Data/test_full.npy')
         self.v_feat = np.load('./Data/FeatureVideo_normal.npy')
         self.a_feat = np.load('./Data/FeatureAudio_avg_normal.npy')
         self.t_feat = np.load('./Data/FeatureText_stl_normal.npy')
+        user_item_dict = np.load('./Data/user_item_dict.npy', allow_pickle=True).item()
+        pos_row = []
+        pos_col = []
+        for key in user_item_dict.keys():
+            items = user_item_dict[key]
+            pos_col.extend(list(items))
+            pos_row.extend([key] * len(items))
+
         print('Data has been loaded.')
         ##########################################################################################################################################
         if self.model_name == 'MMGCN':
             self.features = [self.v_feat, self.a_feat, self.t_feat]
-            self.model = MMGCN(self.features, self.edge_index, self.user_index_5,self.batch_size, self.num_user, self.num_item, self.aggr_mode, self.concat, self.num_layer, self.has_id, self.dim_latent).to(self.device)
+            self.model = MMGCN(self.features, self.edge_index, self.user_index_5,self.batch_size, self.num_user, self.num_item, self.aggr_mode, self.concat, self.num_layer, self.has_id, self.dim_latent,self.weight_decay,pos_row, pos_col).to(self.device)
 
         elif self.model_name == 'VBPR':
             self.dim_feat = self.dim_v+self.dim_a+self.dim_t
@@ -132,20 +140,21 @@ class Net:
             self.model.load_state_dict(torch.load(args.PATH_weight_load))
             print('module weights loaded....')
         ##########################################################################################################################################
-        self.optimizer = torch.optim.Adam([{'params': self.model.parameters(), 'lr': self.learning_rate}], weight_decay=self.weight_decay)
+        self.optimizer = torch.optim.Adam([{'params': self.model.parameters(), 'lr': self.learning_rate}])
         ##########################################################################################################################################
 
     def run(self):
+        max_precision = 0.0
         max_recall = 0.0
-        max_pre = 0.0
-        max_ndcg = 0.0
-        step = 0
+        max_NDCG = 0.0
+        num_decreases = 0
 
-       
-        with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "w") as f:
+        print(args.l_r)
+        print(args.weight_decay)
+        with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "a") as f:
             f.write(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt')
             f.write("\n")
-        early_stopping = EarlyStopping(patience=20, verbose=True)
+        # early_stopping = EarlyStopping(patience=20, verbose=True)
         for epoch in range(self.num_epoch):
             self.model.train()
             print('Now, training start ...')
@@ -153,43 +162,70 @@ class Net:
             sum_loss = 0.0
             for data in self.train_dataloader:
                 self.optimizer.zero_grad()
-                self.loss = self.model.loss(data)
+                self.loss,reg_loss = self.model.loss(data)
                 self.loss.backward()
                 self.optimizer.step()
                 pbar.update(self.batch_size)
                 sum_loss += self.loss
             print(sum_loss/self.batch_size)
             pbar.close()
-
-            if epoch % 1 == 0:
-                print('Validation start...')
-                self.model.eval()
-                with torch.no_grad():
-                    precision, recall, ndcg_score = self.model.accuracy(self.val_dataset, topk=10)
-                    print('---------------------------------Val: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
-                        epoch, 10, precision, recall, ndcg_score))
-                    with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "w") as f:
-                         f.write('---------------------------------Val: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
-                        epoch, 10, precision, recall, ndcg_score))  # 将字符串写入文件中
-                         f.write("\n")
-            early_stopping(precision, self.model)
-
-            if early_stopping.early_stop:
-                with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "w") as f:
-                     f.write("Early stopping")
-                     f.write("\n")
+            if torch.isnan(sum_loss/self.batch_size):
+                with open('./Data/result_{0}_{1}_noselfloog_noact_noweight.txt'.format(args.l_r,args.weight_decay),
+                          'a') as save_file:
+                    save_file.write('lr: {0} \t Weight_decay:{1} is Nan'.format(args.l_r, args.weight_decay))
                 break
 
+            val_precision, val_recall, val_ndcg = full_vt(epoch, self.model, self.val_dataset, 'Val')
+            with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "a") as f:
+                 f.write('---------------------------------Val: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
+                           epoch, 10, val_precision, val_recall, val_ndcg))  # 将字符串写入文件中
+                 f.write("\n")
+            test_precision, test_recall, test_ndcg = full_vt(epoch, self.model, self.test_dataset, 'Test')
+            with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "a") as f:
+                 f.write('---------------------------------test: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
+                           epoch, 10, test_precision, test_recall, test_ndcg))  # 将字符串写入文件中
+                 f.write("\n")
+            if test_recall > max_recall:
+                max_precision = test_precision
+                max_recall = test_recall
+                max_NDCG = test_ndcg
+                num_decreases = 0
+            else:
+                if num_decreases > 20:
+                    with open('./Data/result_{0}_{1}.txt'.format(args.l_r,args.weight_decay), 'a') as save_file:
+                        save_file.write(
+                            'lr: {0} \t Weight_decay:{1} =====> Precision:{2} \t Recall:{3} \t NDCG:{4}\r\n'.
+                            format(args.l_r, args.weight_decay, max_precision, max_recall, max_NDCG))
+                    break
+                else:
+                    num_decreases += 1
+            # if epoch % 1 == 0:
+            #     print('Validation start...')
+            #     self.model.eval()
+            #     with torch.no_grad():
+            #         precision, recall, ndcg_score = self.model.accuracy(self.val_dataset, topk=10)
+            #         print('---------------------------------Val: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
+            #             epoch, 10, precision, recall, ndcg_score))
+            #         with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "w") as f:
+            #              f.write('---------------------------------Val: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
+            #             epoch, 10, precision, recall, ndcg_score))  # 将字符串写入文件中
+            #              f.write("\n")
+            # early_stopping(precision, self.model)
+            #
+            # if early_stopping.early_stop:
+            #     with open(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt', "w") as f:
+            #          f.write("Early stopping")
+            #          f.write("\n")
+            #     break
+            #
+            # if epoch % 5 == 0:
+            #     self.model.eval()
+            #     with torch.no_grad():
+            #         precision, recall, ndcg_score = self.model.accuracy(self.test_dataset, topk=10)
+            #         print('---------------------------------Test: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
+            #             epoch, 10, precision, recall, ndcg_score))
 
-
-            if epoch % 5 == 0:
-                self.model.eval()
-                with torch.no_grad():
-                    precision, recall, ndcg_score = self.model.accuracy(self.test_dataset, topk=10)
-                    print('---------------------------------Test: {0}-th epoch {1}-th top Precition:{2:.4f} Recall:{3:.4f} NDCG:{4:.4f}---------------------------------'.format(
-                        epoch, 10, precision, recall, ndcg_score))
-
-        return max_recall, max_pre, max_ndcg
+        return max_recall, max_precision, max_NDCG
 
 
 if __name__ == '__main__':
@@ -200,23 +236,22 @@ if __name__ == '__main__':
     parser.add_argument('--PATH_weight_load', default=None, help='Loading weight filename.')
     parser.add_argument('--PATH_weight_save', default=None, help='Writing weight filename.')
     parser.add_argument('--l_r', type=float, default=1e-3, help='Learning rate.')
-    parser.add_argument('--weight_decay', type=float, default=1e-3, help='Weight decay.')
+    parser.add_argument('--weight_decay', type=float, default=1e-4, help='Weight decay.')
     parser.add_argument('--batch_size', type=int, default=1024, help='Batch size.')
     parser.add_argument('--dim_latent', type=int, default=64, help='Latent dimension.')
     parser.add_argument('--num_epoch', type=int, default=200, help='Epoch number.')
     parser.add_argument('--num_workers', type=int, default=40, help='Workers number.')
     parser.add_argument('--num_user', type=int, default=55485, help='User number.')
     parser.add_argument('--num_item', type=int, default=5986, help='Item number.')
-    parser.add_argument('--aggr_mode', default='mean', help='Aggregation mode.')
+    parser.add_argument('--aggr_mode', default='add', help='Aggregation mode.')
     parser.add_argument('--concat', type=bool, default=True, help='Concatenation')
-    parser.add_argument('--num_layer', type=int, default=2, help='Layer number.')
+    parser.add_argument('--num_layer', type=int, default=1, help='Layer number.')
     parser.add_argument('--has_id', type=bool, default=True, help='Has id_embedding')
 
 
     args = parser.parse_args()
     # sys.stdout = Logger(f'./train_log_({args.l_r:.6f}_{args.weight_decay:.6f}).txt')
-    print(args.l_r)
-    print(args.weight_decay)
+
     egcn = Net(args)
     egcn.run()    
 
